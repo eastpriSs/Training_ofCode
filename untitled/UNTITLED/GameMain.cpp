2817614eb2d7b47d6d764033034fbadad5ca50ff
eastpriSs <73599984+eastpriSs@users.noreply.github.com>
2022-08-21 21:47:55 +0700

    add object class

--- untitled/UNTITLED/GameMain.cpp
+++ untitled/UNTITLED/GameMain.cpp
@@ -3,29 +3,61 @@
 
 using MatrixStuff::Matrix;
 
-// class Object
-// {
-// private:
+class Object
+{
+private:
+
+    short _pos_x = 0;
+    short _pos_y = 0;
+
+    sf::RectangleShape  *back_rect;
+    sf::CircleShape     *front_shape;
+
+public:
+    Object();
+    void setPos(const short&, const short&) noexcept;
+    void draw_on(sf::RenderWindow&) noexcept;
+    ~Object() = default;
+};
+
+// OBJECT.CPP
+// mtr[i][j].draw_on(win);
+inline void Object::draw_on(sf::RenderWindow& w) noexcept 
+{
+    w.draw(*back_rect);
+    w.draw(*front_shape);
+}
+
+void Object::setPos(const short& x, const short& y) noexcept {
     
+    _pos_x = x;
+    _pos_y = y;
 
-// public:
-//     Object(const int x = 0, const int&);
-//     ~Object();
-// };
+    back_rect->setPosition(_pos_x, _pos_y);
+    front_shape->setPosition(_pos_x, _pos_y);
+}
 
-// Object::Object(const int& x, const int& y)
-// {
-// }
-
-// Object::~Object()
-// {
-// }
-
+Object::Object() 
+: back_rect(new sf::RectangleShape), front_shape(new sf::CircleShape)
+{
+    // Прямоугольник сзади круга
+    back_rect->setSize(sf::Vector2f(50, 50));
+    back_rect->setOutlineColor(sf::Color::White);
+    back_rect->setOutlineThickness(5);
+    back_rect->setPosition(_pos_x, _pos_y);
+    
+    // Круг
+    front_shape->setRadius(25);
+    front_shape->setOutlineColor(sf::Color::Green);
+    front_shape->setOutlineThickness(5);
+    front_shape->setPosition(_pos_x, _pos_y);
+    
+}    
 
 // Главный цикл
 int main()
 {
-    Matrix<sf::CircleShape> brd(4,4);
+    Matrix<Object> brd(4,4);
     Matrix<int> n(3,3);
     n[0][1] = 2;
     n.print();
@@ -36,13 +68,11 @@ int main()
     for (size_t i = 0; i < 4; i++)
     {
         for (size_t j = 0; j < 4; j++) {
-            brd[i][j].setRadius(10.f);
-            brd[i][j].setPosition(pos_x,pos_y);
-            brd[i][j].setFillColor(sf::Color::Red);
-            pos_x += 40;
+            brd[i][j].setPos(pos_x, pos_y);
+            pos_x += 100;
         }    
         pos_x = 10;
-        pos_y += 40;
+        pos_y += 100;
     }
 
     sf::RenderWindow window(sf::VideoMode(800, 800), "SFML works!");
@@ -61,7 +91,7 @@ int main()
 
         for (size_t i = 0; i < 4; i++)
             for (size_t j = 0; j < 4; j++)
-                window.draw(brd[i][j]);
+                brd[i][j].draw_on(window);
 
         window.display();
     }
